321963291689affc407359b0dac53c1a52b40f6d
sam bassett <samson.bassett@gmail.com>
2015-08-26 19:37:27 -0700

    revert UtilExp to use 1.7 numbers

--- src/main/java/com/lothrazar/powerinventory/UtilExperience.java
+++ src/main/java/com/lothrazar/powerinventory/UtilExperience.java
@@ -41,6 +41,19 @@ public class UtilExperience
 	public static int getXpToGainLevel(int level)
 	{
 		//numeric reference: http://minecraft.gamepedia.com/Experience#Leveling_up
+		 //1.7.10
+		if (level >= 30) 
+		{
+		    return 62 + (level - 30) * 7;
+		} 
+		else if (level >= 15) 
+		{
+		    return 17 + (level - 15) * 3;
+		} else 
+		{
+		    return 17;
+		}
+		/* 1.8 numbers
 		//so if our current level is 5, we pass in5 here and find out
 		//how much exp to get from 5 to 6
 		int nextLevelExp = 0;
@@ -52,21 +65,28 @@ public class UtilExperience
 		else //level >= 31 
 			nextLevelExp = 9*level - 158;
 		
-		return nextLevelExp;
+		return nextLevelExp;*/
 	}
 	
 	public static int getXpForLevel(int level)
 	{
 		//numeric reference: http://minecraft.gamepedia.com/Experience#Leveling_up
 		int totalExp = 0;
-		
+		//1.7  http://minecraft.gamepedia.com/Experience#Values_of_1.3.1_-_Before_1.8_.2814w02a.29
+		if(level <= 15)
+			totalExp = level*17;
+		else if(level <= 30)
+			totalExp = (int)(1.5*level*level - 29.5*level + 360);
+		else //level >= 31 
+			totalExp = (int)(3.5*level*level - 151.5*level + 2220);
+		/* 1.8:::
 		if(level <= 15)
 			totalExp = level*level + 6*level;
 		else if(level <= 30)
 			totalExp = (int)(2.5*level*level - 40.5*level + 360);
 		else //level >= 31 
 			totalExp = (int)(4.5*level*level - 162.5*level + 2220);//fixed. was +162... by mistake
-		
+		*/
 		return totalExp;
 	}
 	
