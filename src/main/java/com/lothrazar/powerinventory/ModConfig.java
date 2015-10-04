73da6194bc7ca5c204d3f5bdac86cee649658ebf
sam bassett <samson.bassett@gmail.com>
2015-10-04 07:01:54 -0700

    can change size with ingame config

--- src/main/java/com/lothrazar/powerinventory/ModConfig.java
+++ src/main/java/com/lothrazar/powerinventory/ModConfig.java
@@ -67,8 +67,18 @@ public class ModConfig
 		
 		ModConfig.alwaysShowHungerbar = config.getBoolean("always_show_hunger",category,true,"Always show hunger bar - even while mounted.  Horse health will show above the hunger bar.");
 
+		
+		ModConfig.smallMedLarge = config.getString("main_size", category, "normal", "Valid values are only exactly 'normal', 'small', 'large'.    Changes your inventory size, for use if your GUI Scale requirements are different.  normal = regular 15x25 inventory size, small = 6x18.  WARNING: EMPTY YOUR PLAYERS INVENTORY IN A CHEST before changing this.  And to be safe, BACKUP YOUR WORLD!");
+		
+		
+		// TODO exp cost for uncrafting added (default zero)  
+
+		//TODO enable/disable 3x3 crafting
+		
+		
 		category = "stack_to_64";
 
+		//these dont seem to work without restarting the game
 		ModConfig.enderPearl64 = config.get( category,"ender_pearl", true).getBoolean();
 		ModConfig.minecart64 = config.get(category,"minecarts",  true).getBoolean();
 		ModConfig.boat64 = config.get(category,"boats",  true).getBoolean();
@@ -78,14 +88,14 @@ public class ModConfig
 		ModConfig.bucket64 = config.get(category,"empty_bucket",  true).getBoolean();
 		//?? maybe? record and horse armor?saddle?
 		
-		category = "warning_advanced";
 		
-		config.addCustomCategoryComment(category, "This section is for disabling and changes major features.  Always empty your inventory before changing these.");
+		//category = "warning_advanced";
+		
+		//config.addCustomCategoryComment(category, "This section is for disabling and changes major features.  Always empty your inventory before changing these.");
 		
 		
 		
-		ModConfig.smallMedLarge = config.getString("main_size", category, "normal", "Valid values are only exactly 'normal', 'small', 'large'.    Changes your inventory size, for use if your GUI Scale requirements are different.  normal = regular 15x25 inventory size, small = 6x18.  WARNING: EMPTY YOUR PLAYERS INVENTORY IN A CHEST before changing this.  And to be safe, BACKUP YOUR WORLD!");
-
+		category = Configuration.CATEGORY_GENERAL;//"warning_advanced";
 		ModConfig.blockVersionChecker = config.getBoolean("block_versionchecker",category,false,"By default, this checks once on game startup for a new mod update version.  Set as true to block this check and notice.  Config entry added for modpack creators.");
 
 		
@@ -97,28 +107,10 @@ public class ModConfig
 		
 		ModConfig.enableCompatMode =  config.getBoolean("compatibility_mode",category,false,"False is the regular mod with everything normal.  "
 				+ "True will give you the regular vanilla inventory, not replaced or changed in any way for compatibility reasons.  Instead, push the upper right button to use the mini version.");
-		
-
-		//TODO: more stufffffffffffffffs
-		
-//IDEAS::
-		//2: add in/fix the armor background images
+	 
+	 
 		
 		
-		//3: option to turn off background images (including armor)
-		
-		
-		//4 more 64 stacking
-		
-		//turn onn/off the uncrafting and ehcnat bottling
-		
-		
-		//turn off version checker 
-		
-		// exp cost for uncrafting added (default zero)  
-		
-		//?? enable/disable 3x3 crafting
-		// ???: make the left 9 columns actually go vertically down so it matches player invo
 
 		if(config.hasChanged()){config.save();}
 		
@@ -136,44 +128,32 @@ public class ModConfig
 			BigContainerPlayer.ALL_ROWS = 15;
 			BigContainerPlayer.ALL_COLS = 25;
 
+			
 			GuiBigInventory.texture_width = 464;
 			GuiBigInventory.texture_height = 382;
-		    //GuiBigInventory.backgroundTexture = "textures/gui/inventory_15x25.png";//375 total
-		    
-		    //Const.bottleX = GuiBigInventory.texture_width - Const.SQ - Const.padding - 1;
 		}
-		else if(ModConfig.smallMedLarge.equalsIgnoreCase("large"))
-		{
-			//might as well add more rows too. work in progress
-			
-			BigContainerPlayer.ALL_ROWS = 16;//3 + Const.MORE_ROWS;
-			BigContainerPlayer.ALL_COLS = 28;//9 + Const.MORE_COLS;
-
-			GuiBigInventory.texture_width = 500;
-			GuiBigInventory.texture_height = 400;
-			//GuiBigInventory.backgroundTexture = "textures/gui/inventory_16x28.png";
-		    
-		  //  int offset = 18*2;
-		    //Const.bottleX = GuiBigInventory.texture_width - Const.SQ - Const.padding - 1;
-		    //Const.bottleX += offset; 
-			//Const.uncraftX += offset; 
-		}
-		else//assume its small
-		{
-			//config.setString("normal_small","small");
+		
+		else if(ModConfig.smallMedLarge.equalsIgnoreCase("small"))
+		{ 
 			ModConfig.smallMedLarge = "small";
 			
-			BigContainerPlayer.ALL_ROWS = 6;//3 + Const.MORE_ROWS;
-			BigContainerPlayer.ALL_COLS = 18;//9 + Const.MORE_COLS;
+			BigContainerPlayer.ALL_ROWS = 6; 
+			BigContainerPlayer.ALL_COLS = 18; 
 
 			GuiBigInventory.texture_width = 338;
 			GuiBigInventory.texture_height = 221;
+ 
+		}
+		else //if(ModConfig.smallMedLarge.equalsIgnoreCase("large"))
+		{
+			//default to large BECAUSE< if they have a typo, we do not wan
+			// to make it super small and cause data loss
+			BigContainerPlayer.ALL_ROWS = 16; 
+			BigContainerPlayer.ALL_COLS = 28; 
 
-		    //int offset = 18*7;
-		   // Const.bottleX -= offset; 
-			//Const.uncraftX -= offset; 
-			
-			//6*18 is 108..so yeah?
+			GuiBigInventory.texture_width = 500;
+			GuiBigInventory.texture_height = 400;
+
 		}
 		
 		GuiBigInventory.backgroundTexture = "textures/gui/inventory_" 
