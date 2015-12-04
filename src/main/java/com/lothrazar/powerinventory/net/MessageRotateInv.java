cc8953bca3dbe2fe6b09b59ec134d6dceab04fa6
sam bassett <samson.bassett@gmail.com>
2015-12-03 19:43:25 -0800

    add button and packet for invo swapping

---
+++ src/main/java/com/lothrazar/powerinventory/net/MessageRotateInv.java
@@ -0,0 +1,52 @@
+package com.lothrazar.powerinventory.net;
+ 
+import com.lothrazar.powerinventory.InventoryPersistProperty;
+import com.lothrazar.powerinventory.UtilInventory;
+
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class MessageRotateInv implements IMessage, IMessageHandler<MessageRotateInv, IMessage>
+{
+	NBTTagCompound tags = new NBTTagCompound(); 
+	public MessageRotateInv()	{ 	}
+	public MessageRotateInv(NBTTagCompound ptags)	
+	{
+		tags = ptags; 	
+	}
+	
+	@Override
+	public void fromBytes(ByteBuf buf) 
+	{
+		tags = ByteBufUtils.readTag(buf);
+	}
+
+	@Override
+	public void toBytes(ByteBuf buf) 
+	{
+		ByteBufUtils.writeTag(buf, this.tags);
+	}
+	
+	@Override
+	public IMessage onMessage(MessageRotateInv message, MessageContext ctx)
+	{  
+		EntityPlayer p = ctx.getServerHandler().playerEntity; 
+		
+		InventoryPersistProperty prop = InventoryPersistProperty.get(p);
+		
+		IInventory invo = prop.inventory;
+
+		
+
+		
+		
+		return null;
+	}
+}
+ 
